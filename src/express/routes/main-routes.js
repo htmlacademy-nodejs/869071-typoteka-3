d4fe17f074d9586a3ead0e0c6869154e9198b9cf
Vladimir Vasilevich <vavasilevich@sberbank.ru>
2021-04-08 18:10:48 +0300

    убрал лишний адрес

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -6,7 +6,6 @@ const mainRoutes = new Router();
 // Определяем `GET` маршруты.
 // основной путь маршрута /
 mainRoutes.get(`/`, (req, res) => res.render(`main`));
-mainRoutes.get(`/main`, (req, res) => res.render(`main`));
 mainRoutes.get(`/register`, (req, res) => res.render(`sing-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`sing-up`));
 mainRoutes.get(`/search`, (req, res) => res.render(`search`));
